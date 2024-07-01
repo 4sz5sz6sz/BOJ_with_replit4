7976d82b89d1c8bbc0e11c3066d96772b0c03678
4sz5sz6sz <98520968+4sz5sz6sz@users.noreply.github.com>
2024-07-01 12:56:21 +0000

    240701

---
+++ .ccls-cache/src/silver/bronze/BOJ14581_Hongjun_surrounded_by_fans.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	string name;
+	cin >> name;
+
+	string s1 = ":fan:";
+
+	cout << s1 << s1 << s1 << "\n";
+	cout << s1 << ":" << name << ":" << s1 << "\n";
+	cout << s1 << s1 << s1 << "\n";
+	
+	return 0;
+}
+
+/*
+02:25
+240701 
+*/
